6a1b43d37412d2f4eb9dea92407998647fc66526
wesley oliveira <wlwwesley9@gmail.com>
2022-07-09 12:45:57 -0300

    nuvens param quando o mario colide

--- Js/script.js
+++ Js/script.js
@@ -1,5 +1,6 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
+const clouds = document.querySelector('.clouds');
 
 const jump = () =>{
     mario.classList.add('jump');
@@ -13,12 +14,16 @@ const jump = () =>{
 const loop = setInterval(()=>{
 
     const pipePosition = pipe.offsetLeft;
+    const cloudsPosition = clouds.offsetLeft;
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
 
     if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80){
 
         pipe.style.animation = 'none';
         pipe.style.left= `${pipePosition}px`;
+
+        clouds.style.animation = 'none';
+        clouds.style.left= `${cloudsPosition}px`;
 
         mario.style.animation = 'none';
         mario.style.bottom= `${marioPosition}px`;
